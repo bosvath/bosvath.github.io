6126b1f2a69ce5c7945e54df96e9ebaaa06f4f35
Botond Márk Osváth <45606657+bosvath@users.noreply.github.com>
2021-05-22 22:07:02 +0200

    test

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "osvth.botond@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    }
+?>
+
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -51,6 +69,13 @@
 
 	</head>
 	<body>
+	<form action="" method="post">
+		First Name: <input type="text" name="first_name"><br>
+		Last Name: <input type="text" name="last_name"><br>
+		Email: <input type="text" name="email"><br>
+		Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+		<input type="submit" name="submit" value="Submit">
+	</form>
 	<div id="colorlib-page">
     <div class="container-wrap">
         <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
